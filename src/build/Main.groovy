733847e7932247fd7ae3d00b910f5e9951cdc579
srinivsd <mailsridhar84@gmail.com>
2017-10-01 15:47:00 -0700

    adding stages to the file

--- src/build/Main.groovy
+++ src/build/Main.groovy
@@ -8,6 +8,8 @@ def StartWithFuncton(){
 
 def RunSecondFunction(a){
 	println a
-	checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/gabrielf/maven-samples.git']]])
-	bat "mvn clean install"
+	stage('build'){
+		checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/gabrielf/maven-samples.git']]])
+		bat "mvn clean install"
+	}
 }
