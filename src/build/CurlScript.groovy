76306b135ece2c24af40e44c82527aa3e9e66e93
srinivsd <mailsridhar84@gmail.com>
2017-11-12 14:51:07 -0800

    Update CurlScript.groovy
    Updated Syntax

--- src/build/CurlScript.groovy
+++ src/build/CurlScript.groovy
@@ -5,6 +5,7 @@ def StartWithFunction(){
 		sh 'curl -X POST http://sureshcr862.mylabserver.com:8080/job/LOB2/doDelete --user co23400:273377c5912b65de7facaa1e85f2bd40'
 		println "Deleted Job Succesfully"	
 	}
+}
 def StartWithSecond(){
 	stage('Create Job'){
 		sh 'curl -X POST curl -X POST http://sureshcr862.mylabserver.com:8080/job/Job1_pipeline/createItem?name=DaiSummaJob  --user co23400:273377c5912b65de7facaa1e85f2bd40'
@@ -13,7 +14,6 @@ def StartWithSecond(){
 	}	
 	
 	}
-}
 
 
 
