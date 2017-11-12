72198302900ff1e34980c063300b6a71baf4641b
srinivsd <mailsridhar84@gmail.com>
2017-11-12 14:26:36 -0800

    Update CurlScript.groovy
    Updated curl Script

--- src/build/CurlScript.groovy
+++ src/build/CurlScript.groovy
@@ -1,8 +1,12 @@
 package build
 
 def StartWithFunction(){
-	a = sh 'curl -X POST http://sureshcr862.mylabserver.com:8080/job/TestS3Project/doDelete --user co23400:273377c5912b65de7facaa1e85f2bd40'
-	println "Job Succesful"
+	stage('build'){
+		sh 'curl -X POST http://sureshcr862.mylabserver.com:8080/job/TestS3Project/doDelete --user co23400:273377c5912b65de7facaa1e85f2bd40'
+		println "Job Succesful"
+		
+	}
+	
 	
 }
 
