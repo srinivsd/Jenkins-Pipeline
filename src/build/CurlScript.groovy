1fdeed920f749a206035d92e90be4e6778b2cae1
srinivsd <mailsridhar84@gmail.com>
2017-11-12 14:43:05 -0800

    Update CurlScript.groovy
    Created one more function to create job

--- src/build/CurlScript.groovy
+++ src/build/CurlScript.groovy
@@ -1,12 +1,18 @@
 package build
 
 def StartWithFunction(){
-	stage('build'){
-		sh 'curl -X POST http://sureshcr862.mylabserver.com:8080/job/TestS3Project/doDelete --user co23400:273377c5912b65de7facaa1e85f2bd40'
-		println "Job Succesful"
+	stage('DeleteJob'){
+		sh 'curl -X POST http://sureshcr862.mylabserver.com:8080/job/LOB2/doDelete --user co23400:273377c5912b65de7facaa1e85f2bd40'
+		println "Deleted Job Succesfully"
+		StartWithSecond()
 		
 	}
-	
+def StartWithSecond(){
+	stage('Create Job'){
+		sh 'curl -X POST curl -X POST http://sureshcr862.mylabserver.com:8080/job/Job1_pipeline/createItem?name=DaiSummaJob  --user co23400:273377c5912b65de7facaa1e85f2bd40'
+		println "Created Job Succesful"
+		
+	}	
 	
 }
 
