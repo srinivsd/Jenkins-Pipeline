871f09d0d0c4f591186f928fd2a77b48dc76ffeb
srinivsd <mailsridhar84@gmail.com>
2017-11-12 14:44:55 -0800

    Update CurlScript.groovy
    Corrected Syntax error for the function

--- src/build/CurlScript.groovy
+++ src/build/CurlScript.groovy
@@ -4,8 +4,7 @@ def StartWithFunction(){
 	stage('DeleteJob'){
 		sh 'curl -X POST http://sureshcr862.mylabserver.com:8080/job/LOB2/doDelete --user co23400:273377c5912b65de7facaa1e85f2bd40'
 		println "Deleted Job Succesfully"
-		StartWithSecond()
-		
+		StartWithSecond()	
 	}
 def StartWithSecond(){
 	stage('Create Job'){
@@ -14,6 +13,7 @@ def StartWithSecond(){
 		
 	}	
 	
+	}
 }
 
 
