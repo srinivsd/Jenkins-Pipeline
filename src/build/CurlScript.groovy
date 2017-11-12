b9a05b0285d26c0fa2bf1a16c023c640b9f2d6ba
srinivsd <mailsridhar84@gmail.com>
2017-11-12 14:47:02 -0800

    Update CurlScript.groovy
    removed calling function here

--- src/build/CurlScript.groovy
+++ src/build/CurlScript.groovy
@@ -3,8 +3,7 @@ package build
 def StartWithFunction(){
 	stage('DeleteJob'){
 		sh 'curl -X POST http://sureshcr862.mylabserver.com:8080/job/LOB2/doDelete --user co23400:273377c5912b65de7facaa1e85f2bd40'
-		println "Deleted Job Succesfully"
-		StartWithSecond()	
+		println "Deleted Job Succesfully"	
 	}
 def StartWithSecond(){
 	stage('Create Job'){
